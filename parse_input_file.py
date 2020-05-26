7559d23dcdee81b89cd8a7f10e470ffc30902186
Mihailo Stojanovic <mihailo.stojanovic@rt-rk.com>
2020-05-26 11:04:01 +0200

    Finish implementation of input file parsing
            * parse_input_file.py (report_error): Seek _grammar: section
            instead of _defines:. Add the end($) token to the token list.
            (collect_productions): New function.
            (do_the_magic): Ditto.

--- parse_input_file.py
+++ parse_input_file.py
@@ -1,3 +1,10 @@
+import sys
+from emit_parser import create_header_and_emit_manifest, create_body
+from lr1_collection import create_collection
+from lr1_tables import create_tables
+from process_production import process_production, Terminal
+
+
 # error reporting helper routine
 def report_error(error, line_num):
     print("Line " + str(line_num) + ": ")
@@ -39,11 +46,78 @@ def collect_tokens(file, line_num):
         file_pos = file.tell()
         line = file.readline()
         line_num += 1
-        if not line or "_defines:" in line:
+        if not line or "_grammar:" in line:
             file.seek(file_pos)
             break
 
         token_list.append(line.strip())
 
+    token_list.append('$')
+
     return token_list, line_num
 
+
+# parse the productions
+def collect_productions(file, line_num, terminals, nonterminals, terminals_list):
+    grammar = file.readline().strip()
+    line_num += 1
+    if grammar != "_grammar:":
+        report_error("_grammar: label not found!", line_num)
+
+    current_head = None
+    while True:
+        file_pos = file.tell()
+        line = file.readline()
+        line_num += 1
+        if not line:
+            file.seek(file_pos)
+            break
+
+        line = line.strip()
+        # blank line
+        if line == '':
+            continue
+
+        if line.count("::=") == 1:
+            [current_head, body] = [a.strip() for a in line.split("::=")]
+            process_production(current_head, body, terminals, nonterminals, terminals_list)
+        elif line.startswith('|') and line.count('|') == 1 and current_head is not None:
+            body = line.strip('|')[1].strip()
+            process_production(current_head, body, terminals, nonterminals, terminals_list)
+        else:
+            report_error("Incorrect production!")
+
+
+def do_the_magic(manifest_code, terminals, nonterminals, terminals_list):
+    collection = create_collection(nonterminals, terminals)
+
+    (action_table, goto_table, productions) = create_tables(collection, nonterminals, terminals, terminals_list)
+    create_header_and_emit_manifest(manifest_code, collection, goto_table, action_table, productions)
+    create_body(action_table, goto_table, productions)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        report_error("Incorrect number of command line arguments! Should be only the input file name!", 0)
+
+    filename = sys.argv[1]
+    if ".mlg" not in filename:
+        report_error("Input file name should have .mll extension!", 0)
+
+    with open(filename, 'r') as file:
+        line_num = 0
+
+        terminals = {"eps": Terminal("eps"), "$": Terminal("$")}
+        nonterminals = dict()
+
+        # collect the manifest code
+        (manifest_code, line_num) = collect_manifest_code(file, line_num)
+
+        # collect token definitions
+        (terminals_list, line_num) = collect_tokens(file, line_num)
+
+        # process the grammar productions
+        collect_productions(file, line_num, terminals, nonterminals, terminals_list)
+
+        # create ACTION and GOTO table and emit parser code
+        do_the_magic(manifest_code, terminals, nonterminals, terminals_list)
